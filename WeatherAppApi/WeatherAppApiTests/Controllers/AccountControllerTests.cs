68d1f206343d3ec432ec097565c011499777a0d3
Zscfg <ranoco2@gmail.com>
2018-03-07 16:38:33 +0100

    Add few tests

---
+++ WeatherAppApi/WeatherAppApiTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherAppApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Microsoft.AspNet.Identity;
+using Moq;
+using WeatherAppApi.App_Start;
+using WeatherAppApi.Interfaces;
+using WeatherAppApi.Models;
+
+namespace WeatherAppApi.Controllers.Tests
+{
+    [TestClass()]
+    public class AccountControllerTests
+    {
+        private Mock<IAuthRepository> _authRepositoryMock { get; set; }
+        private AccountController _accountController { get; set; }
+
+        [TestInitialize]
+        public void Startup()
+        {
+            _authRepositoryMock = new Mock<IAuthRepository>();
+            _accountController = new AccountController(_authRepositoryMock.Object);
+        }
+        [TestMethod()]
+        public async Task Register_should_return_ok()
+        {
+           // AutoMapperConfig.Initialize();
+            UserModel user = new UserModel()
+            {
+                UserName = "test",
+                Password = "test",
+                ConfirmPassword = "test"
+            };
+
+            _authRepositoryMock.Setup(x => x.RegisterUser(user)).ReturnsAsync(IdentityResult.Success);
+            var actionResult = await _accountController.Register(user);
+            var contentResult = actionResult as OkNegotiatedContentResult<UserModel>;
+
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(contentResult);
+            Assert.IsInstanceOfType(actionResult, typeof(OkNegotiatedContentResult<UserModel>));
+            Assert.AreEqual(user, contentResult.Content);
+
+        }
+
+
+     
+    }
+}
---
+++ WeatherAppApi/WeatherAppApiTests/Controllers/WeatherControllerTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WeatherAppApi.App_Start;
+using WeatherAppApi.Controllers;
+using WeatherAppApi.Interfaces;
+using WeatherAppApi.Models;
+
+namespace WeatherAppApiTests.Controllers
+{
+    [TestClass]
+    public class WeatherControllerTests
+    {
+        private Mock<IWeatherHistoryRepository> _weatherHistoryRepositoryMock { get; set; }
+        private WeatherController _weatherController;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _weatherHistoryRepositoryMock = new Mock<IWeatherHistoryRepository>();
+            _weatherController = new WeatherController(_weatherHistoryRepositoryMock.Object);
+        }
+
+        [TestMethod]
+        public async Task PostHistory_should_return_ok()
+        {
+            AutoMapperConfig.Initialize();
+
+            var weather = new Weather
+            {
+                Latitude = 20,
+                Longitude = 10,
+                Pressure = 1000,
+                Temperature = -20,
+                WindChill = 20,
+                WindSpeed = 30
+            };
+
+            var actionResult = await _weatherController.PostHistory(weather);
+            Assert.IsInstanceOfType(actionResult, typeof(OkResult));
+        }
+
+        [TestMethod]
+        public async Task PostHistory_should_return_badRequest_when_model_is_null()
+        {
+            Weather weather = null;
+            var actionResult = await _weatherController.PostHistory(weather);
+            Assert.IsInstanceOfType(actionResult, typeof(BadRequestResult));
+        }
+
+
+        [TestMethod]
+        public async Task GetWeatherByUserId_should_return_Ok()
+        {
+            var list = new List<WeatherHistory>
+            {
+                new WeatherHistory
+                {
+                    Latitude = 10,
+                    Longitude = 10,
+                    Date = new DateTime(2000, 10, 20),
+                    Id = "dsad",
+                    Temperature = 10,
+                    Pressure = 20,
+                    WindSpeed = 20,
+                    WindChill = 20
+                },
+                new WeatherHistory
+                {
+                    Latitude = 10,
+                    Longitude = 10,
+                    Date = new DateTime(2000, 10, 20),
+                    Id = "dsad",
+                    Temperature = 10,
+                    Pressure = 20,
+                    WindSpeed = 20,
+                    WindChill = 20
+                }
+            };
+
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Testname"),
+                new Claim(ClaimTypes.NameIdentifier, "1")
+            };
+            var identity = new ClaimsIdentity(claims);
+            IPrincipal user = new ClaimsPrincipal(identity);
+            _weatherController.User = user;
+            _weatherHistoryRepositoryMock.Setup(x => x.GetByUserId(It.IsAny<string>())).ReturnsAsync(list);
+            var actionResult = await _weatherController.GetWeatherByUserId();
+            var contentResult = actionResult as OkNegotiatedContentResult<IEnumerable<WeatherHistory>>;
+
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(contentResult);
+            Assert.IsInstanceOfType(actionResult, typeof(OkNegotiatedContentResult<IEnumerable<WeatherHistory>>));
+            Assert.AreEqual(list, contentResult.Content);
+        }
+
+        [TestMethod]
+        public async Task GetWeatherByUserId_should_return_notFound_when_user_is_not_logged()
+        {
+            var actionResult = await _weatherController.GetWeatherByUserId();
+            
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
+        }
+    }
+
+
+    
+}
