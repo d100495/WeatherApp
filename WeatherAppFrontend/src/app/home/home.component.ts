515e41e736ee03ec7d14329b8915b433b1d79ec9
Zscfg <ranoco2@gmail.com>
2018-04-04 23:49:44 +0200

    Add interactive image for change API

--- WeatherAppFrontend/src/app/home/home.component.ts
+++ WeatherAppFrontend/src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { WeatherFactoryService } from '../Services/WeatherFactoryService';
 
 @Component({
     selector: 'app-home',
-    templateUrl: 'home.component.html'
+    templateUrl: 'home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
