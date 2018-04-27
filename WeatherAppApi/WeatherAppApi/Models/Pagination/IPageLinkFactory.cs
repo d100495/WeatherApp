523f2f3054433524b93a5e8ad86d84889bf9a106
Zscfg <ranoco2@gmail.com>
2018-04-27 14:13:40 +0200

    Refactor PageLinkBuilderFactory

---
+++ WeatherAppApi/WeatherAppApi/Models/Pagination/IPageLinkFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http.Routing;
+using WeatherAppApi.Interfaces;
+
+namespace WeatherAppApi.Models.Pagination
+{
+    public interface IPageLinkFactory
+    {
+        IPageLinkBuilder Create(UrlHelper urlHelper, string routeName, object routeValues, int pageNo,
+            int pageSize, long totalRecordCount);
+    }
+}
